9db2e91e2d761a2e1b1c96f6769da69e277137b0
archit kapoor <wegilant@Wegilants-MacBook-Air.local>
2016-04-22 12:51:54 +0530

    Implemented new code in analyzer.py file

--- analyzer.py
+++ analyzer.py
@@ -1,16 +1,25 @@
+'''
+#=============================================================================
+#     FileName: analyzer.py
+#       Author: Archit Kapoor
+#        Email: archit.imsec10@gmail.com
+#=============================================================================
+'''
+
 import sys
 import os
+import json
 
 
 class Analyzer:
     """
 
-    This is the starting point of the CSV-analyzer.
+    This is the starting point of the CSV-analyzer application.
     
     """
 
-    def __init__(self, remote_json_file_path):
-        self.remote_path_to_json = remote_json_file_path
+    def __init__(self, remote_file_path_to_json):
+        self.remote_path_to_json = remote_file_path_to_json
         self.local_path_to_json = None
 
 
@@ -32,3 +41,20 @@ class Analyzer:
 
     def compare_csv_checksum(self):
         pass
+
+
+def main():
+
+    if len(sys.argv) < 1:
+        print "Usage: <cmd> <remote_file_path_to_json_file> "
+
+        ## Todo call Watchdog to update Error Message in the Db before exiting.
+        sys.exit()
+
+    remote_file_path_to_json = sys.argv[1]
+
+
+
+## Boiler-plate
+if __name__ == "__main__":
+    main()
