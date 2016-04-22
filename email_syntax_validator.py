5bad1cb266ccba632531556a804941c642d81a78
archit kapoor <wegilant@Wegilants-MacBook-Air.local>
2016-04-22 12:12:51 +0530

    Adding email_syntax_validator.py file to the repo.

---
+++ email_syntax_validator.py
@@ -0,0 +1,21 @@
+import re
+import unicodedata
+
+class EmailSyntaxValidator:
+
+    def __init__(self):
+        pass
+
+
+    def validate_by_rfc822(self):
+        pass
+
+
+
+    def validate_by_rfc5321(self):
+        pass
+
+
+
+    def validate_by_rfc6531(self):
+        pass
