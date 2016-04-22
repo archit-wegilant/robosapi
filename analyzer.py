22f76bffd51cc7c535b4cd1bd7881e597ebc2d0b
archit kapoor <wegilant@Wegilants-MacBook-Air.local>
2016-04-22 21:02:12 +0530

    Added some steps to be done after the json file is successfully fetched. Modified the code by instantiating the Analyzer class in the try-except block itself, and calling the fetch_json_from_remote() method there only. Depending on whether json is fetched or not, a True/False value is returned and catched by json_file_fetched_successfully variable.

--- analyzer.py
+++ analyzer.py
@@ -16,7 +16,8 @@ class Analyzer:
     """
 
     This is the starting point of the CSV-analyzer application.
-    This application is initiated by passing in a commond line argument which corresponds to the file path of the json file present at the calling PHP Server.
+    This application is initiated by passing in a commond line argument which corresponds to
+    the file path of the json file present at the calling PHP Server.
     
     """
 
@@ -29,6 +30,10 @@ class Analyzer:
         pass
 
 
+    def fetch_csv_from_remote(self):
+        pass
+
+
     def parse_json(self):
         pass
 
@@ -37,16 +42,12 @@ class Analyzer:
         pass
 
 
-    def fetch_csv(self):
-        pass
-
-
     def compare_csv_checksum(self):
         pass
 
 
 def set_logging_context:
-    ## Todo: Set logging context
+    ## TODO: Set logging context
     pass
 
 
@@ -55,7 +56,7 @@ def main():
     if len(sys.argv) < 1:
         print "Usage: <cmd> <remote_file_path_to_json_file> "
 
-        ## Todo call Watchdog to update Error Message in the Db before exiting.
+        ## TODO call Watchdog to update Error Message in the Db before exiting.
         sys.exit()  ## Check if multiple objects are instantiated as the same time, will it kill all the objects?
 
     remote_file_path_to_json = sys.argv[1]
@@ -64,19 +65,25 @@ def main():
     try:
         ## Try to fetch the remote json file present at the file path given by remote_file_path_to_json
 
-        ## If the json file is fetched, then before leaving the try-except block, set json_file_fetched_successfully to True
-        json_file_fetched_successfully = True
+        if remote_file_path_to_json is not None:    ## Double checking whether remote_file_path_to_json is not None!
+            analyzer_object = Analyzer(remote_file_path_to_json)    ## Instantiating the Analyzer object
+            json_file_fetched_successfully = analyzer_object.fetch_json_from_remote()
+            ## If the json file is fetched, then before leaving the try-except block, set json_file_fetched_successfully to True
+            ## json_file_fetched_successfully = True
+
         pass
         
     except Exception as e:
         print e
-        ## Todo call Watchdog to update Error Message in the Db before exiting.
+        ## TODO call Watchdog to update Error Message in the Db before exiting.
         sys.exit()  ## Check if multiple objects are instantiated as the same time, will it kill all the objects?
     
     if json_file_fetched_successfully:
-
-        analyzer_object = Analyzer(remote_file_path_to_json)    ## Instantiating the Analyzer object
-
+        ## TODO list:
+        ## 1. Parse the fetched json file, which is present locally, by calling parse_json method,
+        ## 2. Match python server id by calling compare_python_server_id method,
+        ## 3. Fetch the csv file from remote by calling fetch_csv_from_remote method,
+        ## 4. Compare the checksum of the csv file, by calling compare_csv_checksum method
         
 ## Boiler-plate
 if __name__ == "__main__":
